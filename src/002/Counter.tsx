2a20bf5e0b907e0170a500b05e2b9411d3769394
ZoungranaHiroko <hiroko@seamaple.co.jp>
2024-08-14 22:15:07 +0900

    end

--- src/002/Counter.tsx
+++ src/002/Counter.tsx
@@ -7,24 +7,27 @@ class Counter extends Component {
 		super(props);
 		this.state = {
 			title : Utils.title,
-			value : 2147483647,
+			value : 0,
 			buttonText1 : '+',
 			buttonText2 : '-'
 		};
 	}
 	onClick1 = () => {
-    this.setState({ value: Utils.add(this.state.value) });
-  };
+    		this.setState({ value: Utils.add(this.state.value) });
+  	};
 	onClick2 = () => {
 		this.setState({ value: Utils.sub(this.state.value) });
 	};
 	render() {
 		return (
 			<div class="container">
-				<p>{this.state.title}</p>
-				<p>{this.state.value}</p>
-				<button onClick={this.onClick1}>{this.state.buttonText1}</button>
-				<button onClick={this.onClick2}>{this.state.buttonText2}</button>
+				<p class="title">{this.state.title}</p>
+				<p class="value">{this.state.value}</p>
+				<div class="btn-area">
+					<button onClick={this.onClick1}>{this.state.buttonText1}</button>
+					&nbsp;&nbsp;
+					<button onClick={this.onClick2}>{this.state.buttonText2}</button>
+				</div>
 			</div>
 		)
 	}
